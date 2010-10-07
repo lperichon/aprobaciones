fce8dfaa182ffe60b3809a19a5d7d7d5e207b2f2
Luis Perichon <luigi@luigi-laptop.(none)>
2010-10-07 15:35:29 -0300

    forgot to fix test

--- spec/helpers/projects_spec.rb
+++ spec/helpers/projects_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ProjectsHelper do
   describe "list_attributes" do
     it "include :name, :owner_email and organization" do
-      !helper.list_attributes(Project.new).should eql([:name, :owner_organization, :owner_email])
+      !helper.list_attributes(Project.new).should eql([:name, :owner_email])
     end
   end
 
